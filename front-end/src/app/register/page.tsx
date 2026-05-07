929f86f81f79568f5262e73970522144ac5a709f
deepak <deepak.n.0603@gmail.com>
2026-05-07 19:29:22 +0530

    removed invalid key from the url

--- front-end/src/app/register/page.tsx
+++ front-end/src/app/register/page.tsx
@@ -41,7 +41,7 @@ export default function RegisterPage() {
     try {
       // Note: Make sure this URL matches your Django urls.py exactly!
       // If your login was /api/login/, your register is probably /api/register/
-      const response = await fetch("https://heart-wise-ihpx.onrender.com/api/login//api/register/", {
+      const response = await fetch("https://heart-wise-ihpx.onrender.com/api/register/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
